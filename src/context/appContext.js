8aab424ca7edb976f1bd7e68f4f28938fd12c767
Ajay Konda <94820332+ajayk134@users.noreply.github.com>
2023-11-22 11:05:30 +0530

    Update appContext.js

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 import React from "react";
-const SOCKET_URL = "https://https://backend-wro9.onrender.com/";
+const SOCKET_URL = "https://backend-wro9.onrender.com/";
 export const socket = io(SOCKET_URL);
 // app context
 export const AppContext = React.createContext();
