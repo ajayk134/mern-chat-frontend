d4d182c8dcde635cb1d4cce10846752d46a1faef
Ajay Konda <94820332+ajayk134@users.noreply.github.com>
2023-11-22 11:04:59 +0530

    Update appApi.js

--- src/services/appApi.js
+++ src/services/appApi.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const appApi = createApi({
     reducerPath: "appApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: "https://back-end-9oa4.onrender.com",
+        baseUrl: "https://backend-wro9.onrender.com/",
     }),
 
     endpoints: (builder) => ({
